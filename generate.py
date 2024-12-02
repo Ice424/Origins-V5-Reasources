0793f1e087fa4f20ee671a11169f8afa8e4244b6
Ice424 <e4elliotd@gmail.com>
2024-12-02 16:17:46 +0000

    refactory of  generate script

--- generate.py
+++ generate.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 os.chdir(Path(__file__).parents[2])
-RESOURCES = os.path.abspath("./resourcepacks/Origins-V5Resources/assets")
+RESOURCES = os.path.abspath("./resourcepacks/Origins-V5-Reasources/assets/")
 DATA = os.path.abspath(
     "./saves/New World/datapacks/Origins-V5-Data/data/chill/powers")
 
@@ -19,16 +19,35 @@ powers = {
 
     }}
 
+
 for path, subdirs, files in os.walk(DATA):
     for name in files:
-        file = (os.path.join(path, name).replace("/home/elliotd/.local/share/ModrinthApp/profiles/OriginsV5/saves/New World/datapacks/Origins-V5-Data/data/chill/powers/",""))
+        file = (os.path.join(path, name).replace(DATA+"/", ""))
         file = file.split("/")
-        print(file)
+
         if file[0] == "class":
-            powers["class"][file[1]].append(str(file[2:]))
+            if file[3].endswith(".json"):
+                #class powers being placed into class key
+                powers["class"][file[1]].append(
+                    str(file[3].replace(".json", "")))
+            else:
+                #preventing double appending with primary & secondary powers
+                if file[4] == "primary.json":
+                    powers["class"][file[1]].append(
+                        str(file[3]))
+
         else:
-            powers[file[0]].append(str(file[1]))
+            #General duplicate prevention for low and high powers as they appear multiple times (different levels)
+            if str(file[1]) not in powers[file[0]]: 
+                powers[file[0]].append(str(file[1]).replace(".json", ""))
+
+for path, subdirs, files in os.walk(os.path.join(RESOURCES+ "/chill/textures/icons/")):
+    for name in files:
+        print(file)
+        file = (os.path.join(path, name).replace(RESOURCES+"/chill/textures/icons/", ""))
+        file = file.split("/")
+        
 
 file = open("/home/elliotd/.local/share/ModrinthApp/profiles/OriginsV5/resourcepacks/Origins-V5-Reasources/powers.json", "w")
-file.write(str(powers))
+file.write(str(powers).replace("'", '"'))
 file.close()
